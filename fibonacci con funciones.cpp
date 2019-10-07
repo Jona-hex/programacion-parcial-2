115096b8f91daa516e32e430e17f1baba7985b6f
Jona-hex <54086084+Jona-hex@users.noreply.github.com>
2019-10-06 21:08:10 -0500

    Funcion fibonacci

---
+++ fibonacci con funciones.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+float fibonnaci(int sum)
+{
+	if (sum==1)
+	{
+ 		return 0;
+	}
+	else if (sum==2)
+ 	{
+ 		return 1;
+ 	}
+ 	else
+ 	{
+ 		return fibonnaci(sum-1)+fibonnaci(sum-2);
+	}
+}
+ int main()
+ {
+ 		
+ 	cin.get();
+ 	cin.get();
+ 	
+ }	
+  
+	
+	 
+ 
